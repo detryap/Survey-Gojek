9b5f641298c60090616b1e3115197ec5c934d895
Detry Ajeng P <detry.ap211298@gmail.com>
2016-10-15 21:07:42 +0700

    firsh commit: membuat desain dan layout

---
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl2010/surveygojek/MainActivity.java
@@ -0,0 +1,27 @@
+package id.sch.smktelkom_mlg.tugas01.xiirpl2010.surveygojek;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+}
